2a3b8189ef39ab7e7d10f8417886159c217289a0
Sebadooo <sebastianhalvarez@hotmail.com>
2021-11-08 02:25:56 -0300

    agregados [ADD Seba] registro]

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var iniciarSesionRouter = require('./routes/iniciarSesion');
+var crearCuentaRouter = require('./routes/crearCuenta');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/iniciarSesion', iniciarSesionRouter);
+app.use('/crearCuenta', crearCuentaRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
