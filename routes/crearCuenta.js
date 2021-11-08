c389b45b3fa5711649b4f8cf437f7a3ac02da302
Sebadooo <sebastianhalvarez@hotmail.com>
2021-11-08 20:51:35 -0300

    subida de lunes [fix] Seba

--- routes/crearCuenta.js
+++ routes/crearCuenta.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* Ir a formulario para inicar sesion. */
+/* Ir a formulario para crear cuenta. */
 router.get('/', function(req, res, next) {
     res.render('crearCuenta', { title: 'Express' });
 });
