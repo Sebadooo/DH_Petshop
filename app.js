d58a6b12c6f0fef5d1dfb4b588941646eda6b8a9
carielgreco <carielgreco@gmail.com>
2021-11-09 23:40:24 -0300

    [ADD-NEW] carrito

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var iniciarSesionRouter = require('./routes/iniciarSesion');
 var crearCuentaRouter = require('./routes/crearCuenta');
+var carrito = require('./routes/carrito');
 
 var app = express();
 
@@ -20,6 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/carrito', carrito);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
